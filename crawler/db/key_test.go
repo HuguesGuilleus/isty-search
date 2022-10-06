dd443018ff60defe877dd2c1da21b90e07198fa7
hugues <ghugues@netc.fr>
2022-10-06 18:17:15 +0200

    Use googleURL in db.TestKeyPath

--- crawler/db/key_test.go
+++ crawler/db/key_test.go
@@ -18,13 +18,9 @@ var googleURL = func() *url.URL {
 }()
 
 func TestKeyPath(t *testing.T) {
-	u, err := url.Parse("https://www.google.com/search/howsearchworks/?fg=1")
-	if err != nil {
-		assert.NoError(t, err)
-	}
-
-	assert.Equal(t, "base/3d/d2/98/199842308839e8f2d7e8f6585154e3ce49e77ccc45340a5b064eacddfe", NewURLKey(u).path("base"))
-	assert.Equal(t, "base/3d/d2/98", NewURLKey(u).dir("base"))
+	assert.Equal(t, "base/3d/d2/98/199842308839e8f2d7e8f6585154e3ce49e77ccc45340a5b064eacddfe",
+		NewURLKey(googleURL).path("base"))
+	assert.Equal(t, "base/3d/d2/98", NewURLKey(googleURL).dir("base"))
 }
 
 func TestObjectBD(t *testing.T) {
