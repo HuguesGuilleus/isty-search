cb341b8a9782113dde5a21b6e489712ce65cbeba
hugues <ghugues@netc.fr>
2022-12-07 19:16:58 +0100

    sloghandlers/ console Handler add internal buffer for the progress bar

--- sloghandlers/console.go
+++ sloghandlers/console.go
@@ -6,12 +6,19 @@ import (
 	"golang.org/x/exp/slog"
 	"io"
 	"os"
-	"strings"
 	"sync"
 )
 
-const barLength = 50
+const (
+	barClean  = "\033[1G\033[K"
+	barLength = 50
+)
 
+// Create a slog.Handler that print log element into the console.
+//
+// Manage console bar:
+//   - "%" and attributes: "%i" and "%len" (optinal)
+//   - "%end", attribute are ignored.
 func NewConsole(level slog.Level) slog.Handler {
 	return &commonHandler{
 		level: level,
@@ -23,14 +30,10 @@ type console struct {
 	outWriter io.Writer
 	outMutex  sync.Mutex
 
-	bar string
+	bar bytes.Buffer
 }
 
 func (c *console) Begin(buff *bytes.Buffer, r slog.Record) bool {
-	if c.bar != "" {
-		buff.WriteString("\033[1G\033[K")
-	}
-
 	switch r.Message {
 	case "%":
 		i := 0
@@ -44,41 +47,45 @@ func (c *console) Begin(buff *bytes.Buffer, r slog.Record) bool {
 			}
 		})
 
+		c.bar.Reset()
+		c.bar.WriteString(barClean)
+
 		percentage := 0
 		if l > 0 {
 			percentage = i * 100 / l
 		}
-		fmt.Fprintf(buff, " %3d %% ", percentage)
+		fmt.Fprintf(&c.bar, " %3d %% ", percentage)
 
 		rate := barLength
 		if l > 0 {
 			rate = i * barLength / l
 		}
 		for i := 0; i < rate; i++ {
-			buff.WriteRune('█')
+			c.bar.WriteRune('█')
 		}
 		for i := rate; i < barLength; i++ {
-			buff.WriteRune('░')
+			c.bar.WriteRune('░')
 		}
-
-		c.bar = strings.Clone(buff.String())
 
 		c.outMutex.Lock()
 		defer c.outMutex.Unlock()
-		buff.WriteTo(c.outWriter)
+		c.outWriter.Write(c.bar.Bytes())
 
 		return true
 
 	case "%end":
-		c.bar = ""
+		c.bar.Reset()
 
 		c.outMutex.Lock()
 		defer c.outMutex.Unlock()
-		c.outWriter.Write([]byte("\033[1G\033[K"))
+		c.outWriter.Write([]byte(barClean))
 
 		return true
 
 	default:
+		if c.bar.Len() != 0 {
+			buff.WriteString(barClean)
+		}
 		fmt.Fprintf(buff, "%s [%s]", r.Level, r.Message)
 		return false
 	}
@@ -86,8 +93,8 @@ func (c *console) Begin(buff *bytes.Buffer, r slog.Record) bool {
 
 func (c *console) Store(buff *bytes.Buffer) error {
 	buff.WriteByte('\n')
-	if c.bar != "" {
-		buff.WriteString(c.bar)
+	if c.bar.Len() != 0 {
+		buff.Write(c.bar.Bytes())
 	}
 
 	c.outMutex.Lock()
