9b409096643d28ac49ade57d803680aa6447e9af
hugues <ghugues@netc.fr>
2023-02-05 22:25:03 +0100

    search/ Add commonKeyFloat32s()

--- search/query.go
+++ search/query.go
@@ -16,6 +16,9 @@ type Query struct {
 
 func parse(q string) []Query {
 	splited := index.GetVocab(q)
+	if len(splited) == 0 {
+		return nil
+	}
 	queries := make([]Query, len(splited))
 	for i, s := range splited {
 		queries[i] = Query{
@@ -24,4 +27,26 @@ func parse(q string) []Query {
 		}
 	}
 	return queries
+}
+
+// Merge common element of the two slice into a.
+// Return the a truncated with common element.
+// The tow slice must be sorted by key.
+func commonKeyFloat32s(a, b []index.KeyFloat32) []index.KeyFloat32 {
+	writeIndex := 0
+	ai := 0
+	bi := 0
+	for ai < len(a) && bi < len(b) {
+		if a[ai].Key == b[bi].Key {
+			a[writeIndex] = a[ai]
+			writeIndex++
+			ai++
+			bi++
+		} else if a[ai].Key.Less(&b[bi].Key) {
+			ai++
+		} else {
+			bi++
+		}
+	}
+	return a[:writeIndex]
 }
--- search/query_test.go
+++ search/query_test.go
@@ -1,6 +1,7 @@
 package search
 
 import (
+	"github.com/HuguesGuilleus/isty-search/index"
 	"github.com/HuguesGuilleus/isty-search/keys"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -11,4 +12,26 @@ func TestParse(t *testing.T) {
 		Query{"hello", keys.NewString("hello"), 0},
 		Query{"word", keys.NewString("word"), 0},
 	}, parse("HELLO WORD!"))
+	assert.Nil(t, parse("aa"))
+}
+
+func TestMergeKeyFloat32(t *testing.T) {
+	assert.Equal(t, []index.KeyFloat32{
+		index.KeyFloat32{keys.Key{1}, 1.1},
+		index.KeyFloat32{keys.Key{3}, 1.3},
+		index.KeyFloat32{keys.Key{6}, 1.6},
+	}, commonKeyFloat32s([]index.KeyFloat32{
+		index.KeyFloat32{keys.Key{1}, 1.1},
+		index.KeyFloat32{keys.Key{2}, 1.2},
+		index.KeyFloat32{keys.Key{3}, 1.3},
+		index.KeyFloat32{keys.Key{4}, 1.4},
+		index.KeyFloat32{keys.Key{6}, 1.6},
+		index.KeyFloat32{keys.Key{7}, 1.7},
+	}, []index.KeyFloat32{
+		index.KeyFloat32{keys.Key{0}, 1.0},
+		index.KeyFloat32{keys.Key{1}, 1.1},
+		index.KeyFloat32{keys.Key{3}, 1.3},
+		index.KeyFloat32{keys.Key{5}, 1.5},
+		index.KeyFloat32{keys.Key{6}, 1.6},
+	}))
 }
